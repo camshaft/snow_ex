7c5c2c578994c3dc64bfbbbd64e054da10e40d74
camshaft <bytheway.cameron@gmail.com>
2015-11-16 23:04:48 -0700

    flatten bad_raw_event

--- lib/snow/model/bad_raw_event.ex
+++ lib/snow/model/bad_raw_event.ex
@@ -2,22 +2,21 @@ defmodule Snow.Model.BadRawEvent do
   def syntax_error(data, parent) do
     %{bad_raw_event(parent) | data: %{
       "line": "",
-      "errors": ["Syntax error #{inspect(data, limit: 5_000)}"]
+      "errors": "Syntax error #{inspect(data, limit: 5_000)}"
     }}
   end
 
   def unknown_schema(%{"schema" => schema}, parent) do
     %{bad_raw_event(parent) | data: %{
       "line": "",
-      "errors": ["Unknown schema #{inspect(schema, limit: 5_000)}"]
+      "errors": "Unknown schema #{inspect(schema, limit: 5_000)}"
     }}
   end
 
   def invalid_data(event, parent) do
-    data = (event["data"] || %{}) |> Poison.encode!()
     %{bad_raw_event(parent) | data: %{
       "line": "",
-      "errors": ["Invalid data for " <> event["schema"] <> ": " <> data]
+      "errors": "Invalid data for #{event["schema"]}: #{inspect(event["data"], limit: 5_000)}"
     }}
   end
 
