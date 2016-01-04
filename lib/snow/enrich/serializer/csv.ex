81e96d7381e9dbaafd0aa23d6f5d0dd017684757
camshaft <bytheway.cameron@gmail.com>
2016-01-04 16:01:48 -0700

    to_list on the CSV output

--- lib/snow/enrich/serializer/csv.ex
+++ lib/snow/enrich/serializer/csv.ex
@@ -3,7 +3,7 @@ defmodule Snow.Enrich.Serializer.CSV do
     stream
     |> Stream.map(fn(item) ->
       name = Snow.Model.name(item)
-      data = [paths.match(name, item)] |> CSV.encode(options)
+      data = [paths.match(name, item)] |> CSV.encode(options) |> Enum.to_list()
       {name, data}
     end)
   end
