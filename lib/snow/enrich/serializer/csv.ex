80c8c1e6ca555154faebb4f1ba7da9248fe92df3
camshaft <bytheway.cameron@gmail.com>
2016-01-16 11:12:08 -0700

    catch UnicodeConversionError in csv serializer

--- lib/snow/enrich/serializer/csv.ex
+++ lib/snow/enrich/serializer/csv.ex
@@ -1,10 +1,18 @@
 defmodule Snow.Enrich.Serializer.CSV do
+  require Logger
+
   def exec(stream, paths) do
     stream
-    |> Stream.map(fn(item) ->
-      name = Snow.Model.name(item)
-      data = paths.match(name, item) |> Enum.join(",")
-      {name, [data, "\r\n"]}
+    |> Nile.expand(fn(item) ->
+      try do
+        name = Snow.Model.name(item)
+        data = paths.match(name, item) |> Enum.join(",")
+        [{name, [data, "\r\n"]}]
+      rescue
+        UnicodeConversionError ->
+          Logger.error("UnicodeConversionError for #{inspect(item, limit: :infinity)}")
+          []
+      end
     end)
   end
 end
